26a9ea471745ed9d65ae25575a1cd061341f5e47
unknown <ghanemkinda@gmail.com>
2024-08-19 15:07:50 +0300

    update

--- src/component/Section7ContactUs/Section7ContactUs.jsx
+++ src/component/Section7ContactUs/Section7ContactUs.jsx
@@ -4,9 +4,12 @@ import ButtonScedule from '../ButtonScedule/ButtonScedule'
 
 export default function Section7ContactUs() {
   return (
-    <section className='SectionContactUs' id='contact'>
+    <>
+    <section  id='contact'>
+        <div className='BGContactUs'>
         <TitleCommon title='Contact US' />
         <p className='ContactPar'>Get In Touch With Our Agent</p>
+        </div>
         <div className='ContactPart2'>
             <div className='ContactItems'>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10044.414642261678!2d36.58099833641784!3d34.459052459588484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15225652b6ab7055%3A0x434575b848dc3fa1!2z2KfZhNiy2LHYp9i52KnYjCDYs9mI2LHZitin!5e0!3m2!1sar!2s!4v1723941485108!5m2!1sar!2s" width="600" height="480"></iframe>
@@ -17,7 +20,7 @@ export default function Section7ContactUs() {
                             <span>010-020-0340</span>
                             <p>Phone Number</p>
                         </div>
-                    </div>
+                    </div> 
                     <div className='ContactCardPart1'>
                         <img src='./public/images/email-icon.png' alt='image-phone'></img>
                         <div>
@@ -40,5 +43,6 @@ export default function Section7ContactUs() {
             </form>
         </div>
     </section>
+    </>
   )
 }
